84c5743024296fccbcb7084fcc8b9249211e802b
leitz <leitz@mikeleitz.com>
2020-11-30 14:53:04 -0500

    Add google analytics 4.

--- src/main.js
+++ src/main.js
@@ -1,9 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import VueGtag from "vue-gtag";
+
 import './assets/tailwind.css'
 
 Vue.config.productionTip = false
+
+Vue.use(VueGtag, {
+  config: { id: "G-85RLFGC4JQ" }
+}, router);
 
 new Vue({
   router,
